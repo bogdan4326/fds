73a51a361d03b0e931bd6347ec449e3ed5267ec6
bogdan4326 <146424895+bogdan4326@users.noreply.github.com>
2024-09-06 20:12:43 +0700

    d

---
+++ csd.py
@@ -0,0 +1,24 @@
+import turtle
+
+#square= turtle.Turtle()
+#square.chape('turtie')
+
+#square.color('red', 'green')
+turtle.shape("turtle")
+turtle.color('red')
+turtle.begin_fill()
+turtle.right(50) 
+turtle.forward(100)
+turtle.left(100)
+turtle.forward(100)
+turtle.left(130)
+turtle.forward(127)
+turtle.end_fill()
+print(turtle.position())
+turtle.penup()
+turtle.goto(0,100)
+turtle.write("привет")
+
+
+
+
