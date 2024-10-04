dc430d76a5cd46291a70b5710de3277016de8e5a
bogdan4326 <146424895+bogdan4326@users.noreply.github.com>
2024-10-04 20:00:57 +0700

    Add files via upload

---
+++ ТЗ_100.py
@@ -0,0 +1,3 @@
+age=input("напиши первое числа")
+age2=input("напиши второе числа")
+print((int(age)+int(age2))/2)
---
+++ ТЗ_104.py
@@ -0,0 +1,8 @@
+#age=input("всего сколько нужно денег ? ")
+q=input("1 друг ")
+w=input("2 друг ")
+e=input("3 друг")
+r=input("4 друг")
+t=input("аренду")
+
+print (int(q)+int(w)+int(e)+int(r)-int(t))
