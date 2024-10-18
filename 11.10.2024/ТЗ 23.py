eb2fe122aebaaff83f3f958de827b7738d1f5212
bogdan4326 <146424895+bogdan4326@users.noreply.github.com>
2024-10-18 15:34:01 +0700

    Add files via upload

---
+++ 11.10.2024/ТЗ 23.py
@@ -0,0 +1,35 @@
+import turtle 
+q=input("какой будет первая линия цвета")
+w=input("какой будет вторая линия цвета")
+e=input("какой будет третья линия цвета")
+r=input("какой будет четвертая линия цвета")
+turtle.begin_fill()
+turtle.fillcolor(q)
+turtle.forward(50)
+turtle.end_fill()
+turtle.begin_fill()
+turtle.fillcolor(w)
+turtle.forward(50)
+turtle.end_fill()
+turtle.begin_fill()
+turtle.fillcolor(e)
+turtle.forward(50)
+turtle.end_fill()
+turtle.begin_fill()
+turtle.fillcolor(r)
+turtle.forward(50)
+turtle.end_fill()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 11.10.2024/ТЗ 4.py
@@ -0,0 +1,4 @@
+age=input("напиши первое числа")
+age2=input("напиши второе числа")
+age3=input("напиши третье числа")
+print(int(age)+int(age2)+int(age3))
